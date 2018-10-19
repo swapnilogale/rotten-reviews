944891028876767e8854afe39ae0a712bf2d05c3
Filype Pereira <pereira.filype@gmail.com>
2018-10-19 22:16:27 +1300

    remove csv export

--- bin/rotten-reviews.js
+++ bin/rotten-reviews.js
@@ -1,30 +1,21 @@
 #!/usr/bin/env node
 
 const Commander = require('commander')
-const Json2CsvParser = require('json2csv').Parser
 const RottenReviews = require('..')
-
-const Csv = new Json2CsvParser({
-  fields: ['reviewer', 'date', 'stars', 'review'],
-})
 
 const description = `Scrapes audience movie or tv show reviews from rotten tomatoes
 
 Examples:
   rotten-reviews venom_2018 100
-  rotten-reviews venom_2018 100 --csv
   rotten-reviews doctor_who/s11 10 --tv   (include the season # for tv shows)`
 
 Commander.description(description)
-  .option('--csv', 'exports to csv (defaults to json)')
   .option('--tv', 'search as a tv show (defaults to movie)')
   .arguments('<title> <count>')
   .action((title, count) => {
     RottenReviews.getAudienceReviews(title, count, Commander.tv)
       .then(reviews => {
-        console.log(
-          Commander.csv ? Csv.parse(reviews) : JSON.stringify(reviews, null, 2)
-        )
+        console.log(JSON.stringify(reviews, null, 2))
       })
       .catch(error => {
         console.error(error.message)
