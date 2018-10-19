b58b356491428b74cee2d1e55daa0a4018be888d
Andrew Fluck <andrew@Andrews-MBP.home>
2018-10-19 17:59:32 -0400

    [feature] add max flag

--- bin/rotten-reviews.js
+++ bin/rotten-reviews.js
@@ -6,20 +6,23 @@ const RottenReviews = require('..')
 const description = `Scrapes audience movie or tv show reviews from rotten tomatoes
 
 Examples:
-  rotten-reviews venom_2018 100
+  rotten-reviews venom_2018
+  rotten-reviews venom_2018 --max 10      (sets max entries to 10)
   rotten-reviews doctor_who/s11 10 --tv   (include the season # for tv shows)`
 
 Commander.description(description)
   .option('--tv', 'search as a tv show (defaults to movie)')
-  .arguments('<title> <count>')
-  .action((title, count) => {
-    RottenReviews.getAudienceReviews(title, count, Commander.tv)
+  .option('--max <maximum>', 'set max entries displayed (defaults to 20)', 20)
+  .arguments('<title>')
+  .action((title) => {
+    RottenReviews.getAudienceReviews(title, Commander.max, Commander.tv)
       .then(reviews => {
         console.log(JSON.stringify(reviews, null, 2))
       })
       .catch(error => {
         console.error(error.message)
       })
+      console.clear()
   })
   .parse(process.argv)
 
