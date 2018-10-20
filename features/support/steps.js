a418f06b3b6af3a457ced611d6c318818925d0ef
Filype Pereira <pereira.filype@gmail.com>
2018-10-20 21:10:25 +1300

    use nock

--- features/support/steps.js
+++ features/support/steps.js
@@ -1,8 +1,13 @@
 const { Given, When, Then } = require('cucumber')
 const expect = require('expect')
+const nock = require('nock')
+
+const baseURL = 'https://www.rottentomatoes.com'
+
 const movieSlugs = {
   'Shutter Island': '1198124-shutter_island'
 }
+
 const reviewSample =
   {
     "reviewer": "Daniel M",
@@ -10,6 +15,40 @@ const reviewSample =
     "stars": 4,
     "review": "Very cool plot twist at the end, enjoyable movie."
   }
+
+const scope = nock(baseURL)
+  .persist()
+  .defaultReplyHeaders({ 'access-control-allow-origin': '*' })
+  .get(`/m/1198124-shutter_island/reviews/`)
+  .query({
+    page: 1,
+    type: 'user'
+  })
+  .reply(200,
+    `<html>
+      <span class="pageInfo">Page 1 of 2</span>
+      <div class="review_table">
+        <div class="row review_table_row">
+          <a class="bold unstyled articleLink">Daniel M</a>
+          <span class="fr small subtle">October 12, 2018</span>
+          <span class="glyphicon glyphicon-star"></span>
+          <span class="glyphicon glyphicon-star"></span>
+          <span class="glyphicon glyphicon-star"></span>
+          <span class="glyphicon glyphicon-star"></span>
+          <div class="user_review">Very cool plot twist at the end, enjoyable movie.</div>
+        </div>
+        <div class="row review_table_row">
+          <a class="bold unstyled articleLink">Ines P</a>
+          <span class="fr small subtle">October 1, 2018</span>
+          <span>
+            <span class="glyphicon glyphicon-star"></span>
+            <span class="glyphicon glyphicon-star"></span>
+            ½
+          </span>
+          <div class="user_review">Uncomfortable length and unrewarding end.</div>
+        </div>
+      </div>
+    </html>`)
 
 Given('that I get the library', function () {
   const { RottenReviews } = this
@@ -22,6 +61,7 @@ When('I try to get the page {string} of the audience reviews of the movie {strin
 })
 
 Then('I see a JSON with a valid result', function () {
+  scope.done()
   const got = Object.keys(this.result)
   const want = Object.keys(reviewSample)
   expect(got).toEqual(want)
